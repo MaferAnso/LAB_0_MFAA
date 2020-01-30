dd6414e25e9903d0bff397af37cfb3b1ccfc5714
MaferAnso <mafer.ansoleaga@gmail.com>
2020-01-30 08:57:14 -0600

    Avances
    hasta el punto 2

---
+++ datos.py
@@ -0,0 +1,9 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Repaso de python y análisis de precios en OANDA                              -- #
+# -- Codigo: Laboratorio_0                                 -- #
+# -- Rep: https://github.com/MaferAnso/LAB_0_MFAA    -- #
+# -- Autor: MaferAnso                                                                 -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+OA_Ak = '7' + '9ae0a52f8e483facdd81f5b316a8ef8-99fb5554f4739c76535b209044f7de2' + '6'
